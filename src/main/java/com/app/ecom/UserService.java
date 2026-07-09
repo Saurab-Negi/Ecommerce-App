a8e20892d9708a3fab866a87f716ffcbd5dfac00
Saurab-Negi <saurabnegi17@gmail.com>
2026-07-09 12:51:46 +0530

    Created User apis (Create, Get, GetById, UpdateById)

--- src/main/java/com/app/ecom/UserService.java
+++ src/main/java/com/app/ecom/UserService.java
@@ -4,17 +4,34 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     private List<User> userList = new ArrayList<>();
+    private Long nextId = 1L;
 
-    public List<User> fetchAllUsers(){
+    public List<User> fetchAllUsers() {
         return userList;
     }
 
-    public List<User> addUser(User user){
+    public Optional<User> fetchUser(Long id) {
+        return userList.stream().filter(user -> user.getId().equals(id)).findFirst();
+    }
+
+    public void addUser(User user) {
+        user.setId(nextId++);
         userList.add(user);
-        return userList;
+    }
+
+    public boolean updateUser(Long id, User updateUser) {
+        return userList.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst()
+                .map(existingUser -> {
+                    existingUser.setFirstName(updateUser.getFirstName());
+                    existingUser.setLastName(updateUser.getLastName());
+                    return true;
+                }).orElse(false);
     }
 }
