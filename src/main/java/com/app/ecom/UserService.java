d57f0a064c72290f550cbf2559382df520f2c456
Saurab-Negi <saurabnegi17@gmail.com>
2026-07-10 12:02:04 +0530

    Created Jpa repository for User

--- src/main/java/com/app/ecom/UserService.java
+++ src/main/java/com/app/ecom/UserService.java
@@ -1,36 +1,37 @@
 package com.app.ecom;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    private List<User> userList = new ArrayList<>();
-    private Long nextId = 1L;
+    private final UserRepository userRepository;
+//    private List<User> userList = new ArrayList<>();
+//    private Long nextId = 1L;
 
     public List<User> fetchAllUsers() {
-        return userList;
+        return userRepository.findAll();
     }
 
     public Optional<User> fetchUser(Long id) {
-        return userList.stream().filter(user -> user.getId().equals(id)).findFirst();
+        return userRepository.findById(id);
     }
 
     public void addUser(User user) {
-        user.setId(nextId++);
-        userList.add(user);
+//        user.setId(nextId++);
+        userRepository.save(user);
     }
 
     public boolean updateUser(Long id, User updateUser) {
-        return userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst()
+        return userRepository.findById(id)
                 .map(existingUser -> {
                     existingUser.setFirstName(updateUser.getFirstName());
                     existingUser.setLastName(updateUser.getLastName());
+                    userRepository.save(existingUser);
                     return true;
                 }).orElse(false);
     }
